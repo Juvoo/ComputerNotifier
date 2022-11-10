b786ed4a647e198d07c4deb690a050d7f0778850
Juvoo <lkinghouser@gmail.com>
2022-11-10 15:06:03 -0600

    Commit

--- app/src/main/java/com/kinghouser/util/NotificationListenerService.java
+++ app/src/main/java/com/kinghouser/util/NotificationListenerService.java
@@ -6,19 +6,37 @@ import android.os.IBinder;
 import android.service.notification.StatusBarNotification;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.net.ServerSocket;
+
 
 public class NotificationListenerService extends android.service.notification.NotificationListenerService {
 
+    public static ServerSocket serverSocket;
+
     @Override
     public IBinder onBind(Intent intent) {
+        prepareNotificationRelayServer();
         return super.onBind(intent);
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            System.out.println("Received notification: " + sbn.getNotification().tickerText + " with timeout " + sbn.getNotification().when);
+            System.out.println("Received notification");
+            NotificationRelay.relayNotification(sbn.getNotification());
         }
         System.out.println(Build.VERSION.SDK_INT);
+    }
+
+    private static void prepareNotificationRelayServer() {
+        try {
+            serverSocket = new ServerSocket(25565);
+
+            while (true) {
+                serverSocket.accept();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/kinghouser/util/NotificationRelay.java
@@ -0,0 +1,10 @@
+package com.kinghouser.util;
+
+import android.app.Notification;
+
+public class NotificationRelay {
+
+    public static void relayNotification(Notification notification) {
+
+    }
+}
